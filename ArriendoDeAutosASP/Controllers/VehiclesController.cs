e7a68023e943940e5d61b164987001c269c52718
Hallowt23 <tomas.hallowt@gmail.com>
2021-06-10 15:30:15 -0400

    Delete

--- ArriendoDeAutosASP/Controllers/VehiclesController.cs
+++ ArriendoDeAutosASP/Controllers/VehiclesController.cs
@@ -27,9 +27,9 @@ namespace ArriendoDeAutosASP.Controllers
         {
             return View();
         }
-
         //Create Post
         [HttpPost]
+        [ValidateAntiForgeryToken]
         public IActionResult Create(Vehicle vehicle)
         {
             if (ModelState.IsValid)
@@ -41,6 +41,64 @@ namespace ArriendoDeAutosASP.Controllers
                 return RedirectToAction("Index");
             }
             return View();
+        }
+        //View Edit
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var vehicle = _context.Vehicle.Find(id);
+            if (vehicle == null)
+            {
+                return NotFound();
+            }
+            return View(vehicle);
+        }
+        //Edit Update
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Vehicle vehicle)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Vehicle.Update(vehicle);
+                _context.SaveChanges();
+                TempData["mssg"] = "Success";
+
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+        //View Delete
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var vehicle = _context.Vehicle.Find(id);
+            if (vehicle == null)
+            {
+                return NotFound();
+            }
+            return View(vehicle);
+        }
+        //Delete Vehicle
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeleteVehicle(int? id)
+        {
+            var vehicle = _context.Vehicle.Find(id);
+            if (vehicle == null)
+            {
+                return NotFound();
+            }
+            _context.Vehicle.Remove(vehicle);
+            _context.SaveChanges();
+            TempData["mssg"] = "Deleted";
+            return RedirectToAction("Index");
         }
     }
 }
