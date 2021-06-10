d6b501952c542ed8511ea6bd185955dc3bfa68f8
Hallowt23 <tomas.hallowt@gmail.com>
2021-06-10 14:34:06 -0400

    Create Vehicle

--- ArriendoDeAutosASP/Controllers/VehiclesController.cs
+++ ArriendoDeAutosASP/Controllers/VehiclesController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ArriendoDeAutosASP.Data;
+using ArriendoDeAutosASP.Models;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,7 +10,20 @@ namespace ArriendoDeAutosASP.Controllers
 {
     public class VehiclesController : Controller
     {
+        private readonly ApplicationDbContext _context;
+
+        public VehiclesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        //Lista Get
         public IActionResult Index()
+        {
+            IEnumerable<Vehicle> listVehicle = _context.Vehicle;
+            return View(listVehicle);
+        }
+        //Create Post
+        public IActionResult Create()
         {
             return View();
         }
