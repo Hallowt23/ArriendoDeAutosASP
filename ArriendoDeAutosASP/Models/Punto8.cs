e7a84098f2f2a2cff909405c8ee1dabaed257b71
Hallowt23 <tomas.hallowt@gmail.com>
2021-06-15 16:56:09 -0400

    Done

---
+++ ArriendoDeAutosASP/Models/Punto8.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ArriendoDeAutosASP.Models
+{
+    public static class Punto8
+    {
+        public static String Function1()
+        {
+            String mssg = " Hola! Yo soy una Public Static String Function." +
+                "Vivo en una clase que no cumple nada mas que mostrarme a mi, " +
+                "Ademas, cumplo la funcion de completar el pto 8 de la prueba n3. " +
+                "Besitos.";
+            return mssg;
+        }
+    }
+}
